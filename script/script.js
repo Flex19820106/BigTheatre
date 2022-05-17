3831bba94342fdf989dff4a62ecfb5ac96820519
Najdenov NA <naidenov_a@mail.ru>
2022-05-17 12:58:28 +0300

    Add media quaeries, and optimized main site

--- script/script.js
+++ script/script.js
@@ -1,7 +1,39 @@
-//let a = document.querySelector('ul');
-//a.addEventListener('click', function(e){
-//	let timer = setInterval (function(){
-//		e.PreventDefault();
-		
-//	})
-//});
+let a = document.querySelectorAll('.btn');
+
+for (let i = 0; i < a.length; i++) {
+	a[i].addEventListener('click', function (e) {
+		let block1 = document.querySelector('.poster1');
+		let block2 = document.querySelector('.poster2');
+
+		function reverse_pictures() {
+			let block1_img_path = block1.querySelector('img').getAttribute('src');
+			let block2_img_path = block2.querySelector('img').getAttribute('src');
+			block2.querySelector('img').setAttribute('src', block1_img_path);
+			block1.querySelector('img').setAttribute('src', block2_img_path);
+		}
+
+		if (this.id == "btn1") {
+			block1.classList.add('animation_poster1_prev');
+			block2.classList.add('animation_poster2_prev');
+			this.setAttribute('disabled', 'disabled');
+			window.setTimeout(function () {
+				console.log("inside setInterval");
+				block1.classList.remove('animation_poster1_prev');
+				block2.classList.remove('animation_poster2_prev');
+				reverse_pictures();
+				document.querySelector('#btn1').removeAttribute('disabled');
+			}, 500);
+		}
+		else {
+			block2.classList.add('animation_poster2_next');
+			block1.classList.add('animation_poster1_next');
+			this.setAttribute('disabled', 'disabled');
+			window.setTimeout(function () {
+				block2.classList.remove('animation_poster2_next');
+				block1.classList.remove('animation_poster1_next');
+				reverse_pictures();
+				document.querySelector('#btn2').removeAttribute('disabled');
+			}, 500);
+        }
+    })
+};
